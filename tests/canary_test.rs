e7227fdf08627698a8279dc489fc53dea1e5fdec
Paul Scoropan <1paulscoropan@gmail.com>
2024-09-17 17:28:20 -0400

    proper integration test fix
    Signed-off-by: Paul Scoropan <1paulscoropan@gmail.com>

--- tests/canary_test.rs
+++ tests/canary_test.rs
@@ -1,21 +1,31 @@
 use std::sync::Arc;
 
 use axum_test::TestServer;
-use hyper::StatusCode;
-
 use fms_guardrails_orchestr8::{
+    config::OrchestratorConfig,
     orchestrator::Orchestrator,
     server::{get_health_app, ServerState},
 };
+use hyper::StatusCode;
+use serde_json::Value;
 
 /// Checks if the health endpoint is working
 #[tokio::test]
 async fn test_health() {
-    let orchestrator = Orchestrator::default();
+    let config = OrchestratorConfig::load("config/test.config.yaml")
+        .await
+        .unwrap();
+    let orchestrator = Orchestrator::new(config, false).await.unwrap();
     let shared_state = Arc::new(ServerState::new(orchestrator));
     let server = TestServer::new(get_health_app(shared_state)).unwrap();
     let response = server.get("/health").await;
+    println!("{:#?}", response);
+    let body: Value = serde_json::from_str(response.text().as_str()).unwrap();
+    println!("{}", serde_json::to_string_pretty(&body).unwrap());
     response.assert_status(StatusCode::OK);
     let response = server.get("/info").await;
+    println!("{:#?}", response);
+    let body: Value = serde_json::from_str(response.text().as_str()).unwrap();
+    println!("{}", serde_json::to_string_pretty(&body).unwrap());
     response.assert_status(StatusCode::OK);
 }
